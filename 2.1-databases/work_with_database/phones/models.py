c972d4838210c15da989c1bd1b8f21a390c4487a
Olya Tatianina <olya.tatianina@gmail.com>
2023-02-27 16:18:05 +0400

    initial commit

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,9 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=60)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.CharField(max_length=200)
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(unique=True)
