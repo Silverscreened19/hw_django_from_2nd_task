5c874a9feba8706bc715e06cd9de6c46a828df97
Olya Tatianina <olya.tatianina@gmail.com>
2023-03-18 16:54:27 +0400

    2nd task

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -5,11 +5,16 @@ from .models import Student
 
 
 def students_list(request):
+    ordering = 'group'
+    object_list = Student.objects.all().prefetch_related('teachers').order_by(ordering)
+    # object_list = Student.objects
     template = 'school/students_list.html'
-    context = {}
+    context = {
+        'object_list': object_list
+    }
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = 'group'
+
 
     return render(request, template, context)
--- 2.2-databases-2/orm_migrations/website/settings.py
+++ 2.2-databases-2/orm_migrations/website/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -36,10 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'school',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -126,8 +133,9 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-try:
-    from .settings_local import *
-except ImportError:
-    pass
+DEFAULT_AUTO_FIELD = 'django.db.models.SmallAutoField'
 
+# try:
+#     from .settings_local import *
+# except ImportError:
+#     pass
